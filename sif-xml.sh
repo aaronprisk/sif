168de159dd65a2b3e748f9304de09941608cb3a0
Aaron Prisk <aaronprisk@gmail.com>
2021-04-28 12:38:49 -0400

    Clear vm xml directory before export

--- sif-xml.sh
+++ sif-xml.sh
@@ -4,6 +4,9 @@
 # Import SIF Config
 source /opt/sif/sif.conf
 
+# Clear previous VM XML exports
+rm $SHAREDIR/sifxml/$HOSTID/$x/*
+
 # Export persistent VM XML to sharedir as defined in sif.conf
 for x in $(virsh list --persistent --name);
 do
